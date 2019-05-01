7098e0fb38161bfee67233e65cfed48ac99ce991
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-04-30 21:26:59 -0300

    RequestService: using properties instead constants and minor refactoring

--- src/main/java/com/globo/pepe/chapolin/services/RequestService.java
+++ src/main/java/com/globo/pepe/chapolin/services/RequestService.java
@@ -1,22 +1,32 @@
 package com.globo.pepe.chapolin.services;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.globo.pepe.common.services.JsonLoggerService;
-import org.asynchttpclient.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.asynchttpclient.util.HttpConstants;
-import org.springframework.stereotype.Component;
-
+import com.globo.pepe.common.services.JsonLoggerService;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Dsl;
+import org.asynchttpclient.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Component
 public class RequestService {
 
-    private AsyncHttpClient asyncHttpClient = Dsl.asyncHttpClient();
-    private String responseBody;
-    private String url = "http://admin.pepe.dev.globoi.com";
+    private static final String ST2_API_KEY_HEADER  = "St2-Api-Key";
+
+    @Value("${pepe.chapolin.stackstorm.url}")
+    private String stackStormUrl;
+
+    @Value("${pepe.chapolin.stackstorm.key}")
+    private String stackStormKey;
+
+    private AsyncHttpClient asyncHttpClient = Dsl.asyncHttpClient(Dsl.config()
+        .setConnectionTtl(10000)
+        .setPooledConnectionIdleTimeout(5000)
+        .setMaxConnections(10)
+        .build());
 
     private final ObjectMapper mapper;
     private final JsonLoggerService jsonLoggerService;
@@ -26,24 +36,16 @@ public class RequestService {
         this.jsonLoggerService = jsonLoggerService;
     }
 
-    public void closeAsyncHttpClient(){
-        try {
-            this.asyncHttpClient.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public String getResponseBody() throws ExecutionException, InterruptedException {
-        return asyncHttpClient.prepareGet("http://admin.pepe.dev.globoi.com").execute().get().getResponseBody();
+        return asyncHttpClient.prepareGet(stackStormUrl).execute().get().getResponseBody();
 
     }
 
-    public Boolean checkIfTriggerExists(String triggerPath, String triggerName) throws ExecutionException, InterruptedException {
-        String checkTriggerURL = String.format("%s%s%s", url,triggerPath,triggerName);
+    public Boolean checkIfTriggerExists(String triggerName) throws ExecutionException, InterruptedException {
+        String checkTriggerURL = stackStormUrl + "/api/triggertypes/" + triggerName;
         System.out.println(checkTriggerURL);
         String response = asyncHttpClient.prepareGet(checkTriggerURL)
-                .addHeader("St2-Api-Key", "ZjAxM2JlNzk0MjJlMmQyYTE2ZjY2ZDBlNjZjODJiODk2YTk5MzEwODdkZDIzMjUzMDQ1ZTZhZWM4OTZlZjAyMQ")
+                .addHeader(ST2_API_KEY_HEADER, stackStormKey)
                 .execute()
                 .get()
                 .getResponseBody();
@@ -51,20 +53,17 @@ public class RequestService {
         try {
             return (ref = mapper.readTree(response).get("ref")) != null && triggerName.equals(ref.asText());
         } catch (IOException e) {
-            jsonLoggerService.newLogger(getClass()).put("short_message", e.getMessage()).sendError();
+            jsonLoggerService.newLogger(getClass()).message(e.getMessage()).sendError();
             e.printStackTrace();
         }
         return false;
     }
 
-    public boolean createTrigger(String triggerCreatePath, String triggerName) throws ExecutionException, InterruptedException {
-        String createTriggerURL = String.format("%s%s", url,triggerCreatePath);
-        System.out.println(createTriggerURL);
-        ObjectNode objectNode = mapper.createObjectNode();
-        objectNode.put("trigger", triggerName).set("payload", mapper.createObjectNode().put("attribute1", "value1"));
+    public boolean createTrigger(String triggerName, JsonNode schema) throws ExecutionException, InterruptedException {
+        String createTriggerURL = stackStormUrl + "/v1/webhooks/st2";
         Response response = asyncHttpClient.preparePost(createTriggerURL)
-                .addHeader("St2-Api-Key", "ZjAxM2JlNzk0MjJlMmQyYTE2ZjY2ZDBlNjZjODJiODk2YTk5MzEwODdkZDIzMjUzMDQ1ZTZhZWM4OTZlZjAyMQ")
-                .setBody(((JsonNode)objectNode).toString())
+                .addHeader(ST2_API_KEY_HEADER, stackStormKey)
+                .setBody((schema).toString())
                 .execute()
                 .get();
         return response.getStatusCode() == 201;
