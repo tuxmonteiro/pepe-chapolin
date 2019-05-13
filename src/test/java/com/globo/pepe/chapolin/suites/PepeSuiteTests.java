6b21a5e836c5ebd029d6c80dcd6196162450fb26
Luiz Henrique Andrade <luizhenrique.andrade@corp.globo.com>
2019-05-13 15:28:00 -0300

    Update PepeSuiteTests to open RabbitMQ Mock port

--- src/test/java/com/globo/pepe/chapolin/suites/PepeSuiteTests.java
+++ src/test/java/com/globo/pepe/chapolin/suites/PepeSuiteTests.java
@@ -1,5 +1,6 @@
 package com.globo.pepe.chapolin.suites;
 
+import com.globo.pepe.chapolin.services.QueueRegisterServiceTests;
 import com.globo.pepe.chapolin.services.RequestServiceTests;
 import com.globo.pepe.chapolin.services.StackstormServiceTests;
 import org.junit.AfterClass;
@@ -11,29 +12,43 @@ import org.mockserver.integration.ClientAndServer;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         StackstormServiceTests.class,
-        RequestServiceTests.class
+        RequestServiceTests.class,
+        QueueRegisterServiceTests.class
 })
 public class PepeSuiteTests {
 
     public static ClientAndServer mockServer;
+    public static ClientAndServer mockServerRabbit;
 
     @BeforeClass
     public static void setup() {
         startMockserver();
+        startMockServerRabbit();
     }
 
     @AfterClass
     public static void cleanup() {
         stopMockServer();
+        stopMockServerRabbit();
     }
 
     public static void startMockserver() {
         mockServer = ClientAndServer.startClientAndServer(9101);
     }
 
+    public static void startMockServerRabbit() {
+        mockServerRabbit = ClientAndServer.startClientAndServer(15672);
+    }
+
     public static void stopMockServer() {
         if (mockServer.isRunning()) {
             mockServer.stop();
+        }
+    }
+
+    public static void stopMockServerRabbit() {
+        if (mockServerRabbit.isRunning()) {
+            mockServerRabbit.stop();
         }
     }
 }
